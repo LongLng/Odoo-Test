07274c4ff1293a1c4ee1916c82e17dacc79910bc
ngoclongcoder <ngoclong.coder@gmail.com>
2023-01-17 17:47:41 +0700

    Update 17/1/2023 i with '#' will be ignored, and an empty message aborts the commit.

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -19,6 +19,7 @@
         'project',
     ],
     'data': [
+        'views/patient.xml',
     ],
     'demo': [
     ],
--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,6 +1,10 @@
-from odoo import models
+from odoo import models, fields
 
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient Record'
+    patient_name = fields.Char(string='Name', required=True)
+    patient_age = fields.Integer(string='Age')
+    notes = fields.Text(string='Notes')
+    image = fields.Binary(string='Image')
